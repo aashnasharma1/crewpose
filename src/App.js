513850f2136c7c568ab275871171c5213aae408b
Aashna Sharma <aashnasharma@Aashnas-MacBook-Air.local>
2025-02-12 03:22:07 +0530

    Initial commit - Crewpose Coming Soon Page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ComingSoon from './Components/comingSoon';
+
+function App() {
+  return (
+    <div className="App">
+      <ComingSoon/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/comingSoon.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "../Styles/comingSoon.css";
+
+const ComingSoon = () => {
+
+  return (
+    <div className="landing-container">
+      <h1 className="brand-name">Crewpose</h1>
+      <h2 className="coming-soon">Coming Soon</h2>
+      <p className="subtext">🔥 Exclusive Anime Oversized T-Shirts 🔥</p>
+   
+    </div>
+  );
+};
+
+export default ComingSoon;
